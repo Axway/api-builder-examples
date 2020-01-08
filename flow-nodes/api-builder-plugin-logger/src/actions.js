02a9e21c9cb2e6939a25b497c3d8afd16ec00964
Alasdair Hurst <ahurst@axway.com>
2020-01-08 15:55:26 +0000

    update package descriptions

--- flow-nodes/api-builder-plugin-logger/src/actions.js
+++ flow-nodes/api-builder-plugin-logger/src/actions.js
@@ -6,10 +6,11 @@
  * @param {object} outputs - A set of output callbacks.  Use it to singal an
  *	 event and pass the output result back to the runtime.  Only use an
  *	 output callback once and only after all asyncronous tasks complete.
+ * @param {object} options - additional paramters provided from the flow engine
  * @return {undefined} Callback
  */
-function log(req, outputs) {
-	const logger = arguments[2].logger;
+function log(req, outputs, options) {
+	const logger = options.logger;
 	let level = req.params.level;
 	if (!level) {
 		level = 'trace';
