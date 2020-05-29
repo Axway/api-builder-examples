643c28c1700d9cec02417710de38e292ac2c952b
dvijayakumar <dvijayakumar@axway.com>
2020-05-29 12:04:06 +0100

    rdpp-6423

--- flow-nodes/api-builder-plugin-querystring/test/test.js
+++ flow-nodes/api-builder-plugin-querystring/test/test.js
@@ -1,22 +1,26 @@
 const getPlugin = require('../src');
 const actions = require('../src/actions');
 const expect = require('chai').expect;
-const { MockRuntime } = require('@axway/api-builder-sdk');
+const { MockRuntime } = require('@axway/api-builder-test-utils');
 
 describe('flow-node querystring', () => {
-	let runtime;
-	before(async () => runtime = new MockRuntime(await getPlugin()));
+	let plugin;
+	let flowNode;
+	before(async () => {
+		plugin = await MockRuntime.loadPlugin(getPlugin);
+		flowNode = plugin.getFlowNode('querystring');
+	});
 
 	describe('#constructor', () => {
 		it('should define flow-nodes', () => {
 			expect(actions).to.be.an('object');
 			expect(actions.encode).to.be.a('function');
-			expect(runtime).to.exist;
-			const flownode = runtime.getFlowNode('querystring');
-			expect(flownode).to.be.a('object');
-			expect(flownode.name).to.equal('Querystring');
-			expect(flownode.description).to.equal('The querystring flow-node provides utilities for parsing and formatting URL query strings.');
-			expect(flownode.icon).to.be.a('string');
+			expect(plugin).to.be.an('object');
+			expect(flowNode).to.be.a('object');
+			expect(flowNode.name).to.equal('Querystring');
+			expect(flowNode.description).
+				to.equal('The querystring flow-node provides utilities for parsing and formatting URL query strings.');
+			expect(flowNode.icon).to.be.a('string');
 		});
 
 		// It is vital to ensure that the generated node flow-nodes are valid
@@ -24,58 +28,42 @@ describe('flow-node querystring', () => {
 		// validation to avoid potential issues when API Builder loads your
 		// node.
 		it('should define valid flow-nodes', () => {
-			expect(runtime.validate()).to.not.throw;
+			// if this is invalid, it will throw and fail
+			plugin.validate();
 		});
 	});
 
 	describe('#encode', () => {
 		it('should error when missing parameter', async () => {
-			const flowNode = runtime.getFlowNode('querystring');
-			const result = await flowNode.encode({obj: null});
-			expect(result.callCount).to.equal(1);
-			expect(result.output).to.equal('error');
-			expect(result.args[1]).to.be.instanceOf(Error)
+			const { value, output } = await flowNode.encode({obj: null});
+			expect(output).to.equal('error');
+			expect(value).to.be.instanceOf(Error)
 				.and.to.have.property('message', 'Missing required parameter: obj');
 		});
 
 		it('should succeed with valid argument', async () => {
-			const flowNode = runtime.getFlowNode('querystring');
 			const obj = { foo: 'bar', baz: ['qux', 'quux'], corge: '' };
-			const result = await flowNode.encode({ obj: obj });
-			expect(result.callCount).to.equal(1);
-			expect(result.output).to.equal('next');
-			expect(result.args).to.deep.equal([ null, 'foo=bar&baz=qux&baz=quux&corge=' ]);
-			expect(result.context).to.deep.equal({
-				encodedString: 'foo=bar&baz=qux&baz=quux&corge='
-			});
+			const { value, output } = await flowNode.encode({ obj: obj });
+			expect(output).to.equal('next');
+			expect(value).to.equal('foo=bar&baz=qux&baz=quux&corge=');
 		});
 	});
 
 	describe('#decode', () => {
 		it('should error when missing parameter', async () => {
-			const flowNode = runtime.getFlowNode('querystring');
-			const result = await flowNode.decode({str: null});
-			expect(result.callCount).to.equal(1);
-			expect(result.output).to.equal('error');
-			expect(result.args[1]).to.be.instanceOf(Error)
+			const { value, output } = await flowNode.decode({str: null});
+			expect(output).to.equal('error');
+			expect(value).to.be.instanceOf(Error)
 				.and.to.have.property('message', 'Missing required parameter: str');
 		});
 
 		it('should succeed with valid argument', async () => {
-			const flowNode = runtime.getFlowNode('querystring');
 			const str = 'foo=bar&abc=xyz&abc=123';
-			const result = await flowNode.decode({ str: str });
-			expect(result.callCount).to.equal(1);
-			expect(result.output).to.equal('next');
-			expect(result.args).to.deep.equal([ null, {
+			const { value, output } = await flowNode.decode({ str: str });
+			expect(output).to.equal('next');
+			expect(value).to.deep.equal({
 				foo: 'bar',
 				abc: ['xyz', '123']
-			}]);
-			expect(result.context).to.deep.equal({
-				decodedObject: {
-					foo: 'bar',
-					abc: ['xyz', '123']
-				}
 			});
 		});
 	});	
